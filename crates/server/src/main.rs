a5de66850eeff1c722ffd014a9ef925add7c859c
sxhk0 <nakamidev@gmail.com>
2024-03-16 02:26:49 -0300

    feat: handle connections inside a async task

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -10,14 +10,16 @@ async fn main() {
         .expect("Failed to bind at port {}");
     
     while let Ok((stream, address)) = listener.accept().await {
-        let mut connection = Connection::from(stream);
-        let message = Message::<String> { 
-            payload: String::from("hello")
-        };
+        tokio::spawn(async move {
+            let mut connection = Connection::from(stream);
+            let message = Message::<String> { 
+                payload: String::from("hello")
+            };
 
-        match connection.send(&message).await {
-            Ok(bytes_written) => println!("Write {:?} bytes to {}", bytes_written, address),
-            Err(error) => eprintln!("Failed to write to {}: {:?}", address, error)
-        }
+            match connection.send(&message).await {
+                Ok(bytes_written) => println!("Write {:?} bytes to {}", bytes_written, address),
+                Err(error) => eprintln!("Failed to write to {}: {:?}", address, error)
+           }
+        });
     }
 }
